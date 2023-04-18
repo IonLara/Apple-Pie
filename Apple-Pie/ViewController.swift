2217d0e70623c97eaaa97b572a0d8c3738936316
SebasMac <ionrodriguezlara@gmail.com>
2023-04-18 14:23:48 -0700

    Set up images and buttons

--- Apple-Pie/ViewController.swift
+++ Apple-Pie/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"]
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0
+    var totalLoses = 0
+    
+    @IBOutlet weak var treeImageView: UIImageView!
+    
+    @IBOutlet weak var scoreLabel: UIStackView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
 
-
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    func newRound() {
+        
+    }
 }
 
